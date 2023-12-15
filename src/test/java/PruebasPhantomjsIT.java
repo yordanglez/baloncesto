5b373bed7f558a32bdbf1010943a0e60ab90b72f
yordanglez <yorda891216@gmail.com>
2023-12-15 18:22:15 +0100

    Prueba local fallida

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -7,19 +7,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PruebasPhantomjsIT
 {
-private static WebDriver driver=null;
-@Test
-public void tituloIndexTest()
-{
-DesiredCapabilities caps = new DesiredCapabilities();
-caps.setJavascriptEnabled(true);
-caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/bin/phantomjs");
-caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, newString[] {"--web-security=no", "--ignore-ssl-errors=yes"});
-driver = new PhantomJSDriver(caps);
-driver.navigate().to("http://localhost:8080/Baloncesto/");
-assertEquals("Votacion mejor jugador liga ACB", driver.getTitle(),"El titulo no es correcto");
-System.out.println(driver.getTitle());
-driver.close();
-driver.quit();
-}
+    private static WebDriver driver=null;
+
+    @Test
+    public void tituloIndexTest()
+    {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, newString[] {"--web-security=no", "--ignore-ssl-errors=yes"});
+        driver = new PhantomJSDriver(caps);
+        driver.navigate().to("http://localhost:8085/Baloncesto/");
+        assertEquals("Votacion mejor jugador liga ACB", driver.getTitle(),"El titulo no es correcto");
+        System.out.println(driver.getTitle());
+        driver.close();
+        driver.quit();
+    }
 }
